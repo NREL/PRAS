3d38393cd1c193c0f44109c914bd7a505fabe5f0
Gord Stephen <gord.stephen@nrel.gov>
2018-07-05 09:06:18 -0600

    Remove old to_energy method

--- src/utils/units.jl
+++ src/utils/units.jl
@@ -26,10 +26,5 @@ unitsymbol(::Type{Year}) = "y"
 #      in terms of conversions to a common set of units (MW, MWh, Hour?)
 #      and ship all conversions through those?
 
-# TODO: Deprecate in favour of `powertoenergy`
-#       (prescribe energy units at the data level and take as an input)
-to_energy(p::Real, ::Type{MW}, n::Real, ::Type{Hour})   = (n*p, MWh)
-to_energy(p::Real, ::Type{MW}, n::Real, ::Type{Minute}) = (n*p/60, MWh)
-
 powertoenergy(p::Real, n::Real, ::Type{Hour}, ::Type{MW}, ::Type{MWh})   = n*p
 powertoenergy(p::Real, n::Real, ::Type{Minute}, ::Type{MW}, ::Type{MWh}) = n*p/60
