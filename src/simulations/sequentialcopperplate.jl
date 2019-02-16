d2421d15ffe3af8b0fcc01e41b102ed94a3e3af7
Gord Stephen <gord.stephen@nrel.gov>
2019-02-15 17:44:44 -0700

    Integrate new result accumulation system

--- src/simulations/sequentialcopperplate.jl
+++ src/simulations/sequentialcopperplate.jl
@@ -10,13 +10,15 @@ end
 iscopperplate(::SequentialCopperplate) = true
 
 function assess_singlesequence!(
-    shortfalls::AbstractVector{V},
-    rng::MersenneTwister,
+    acc::ResultAccumulator,
     extractionspec::Backcast, #TODO: Generalize
     simulationspec::SequentialCopperplate,
-    resultspec::MinimalResult, #TODO: Generalize
-    sys::SystemModel{N1,T1,N2,T2,P,E,V}
+    sys::SystemModel{N1,T1,N2,T2,P,E,V},
+    i::Int
 ) where {N1,T1,N2,T2,P,E,V}
+
+    rng = acc.rng[Threads.threadid()]
+    sample = SystemOutputStateSample(Vector{Tuple{Int,Int}}[], 1)
 
     # Initialize generator and storage state vector
     # based on long-run probabilities from period 1
@@ -31,67 +33,85 @@ function assess_singlesequence!(
     for (t, (gen_set, stor_set)) in enumerate(zip(
         sys.timestamps_generatorset, sys.timestamps_storageset))
 
+        gens = view(sys.generators, :, gen_set)
+        stors = view(sys.storages, :, stor_set)
+
+        update_availability!(rng, gens_available, gens)
+        update_availability!(rng, stors_available, stors)
+        decay_energy!(stors_energy, stors)
+
+        dispatchable_gen_available = available_gen_capacity(gens_available, gens)
         netload = sum(view(sys.load, :, t)) - sum(view(sys.vg, :, t))
 
-        dispatchable_gen_available = available_capacity!(
-            rng, gens_available, view(sys.generators, :, gen_set))
-
-        # println("Net Load: ", netload, "\t",
-        #         "Dispatchable Gen: ", dispatchable_gen_available)
         residual_generation = dispatchable_gen_available - netload
 
         if residual_generation >= 0
 
             # Charge to consume residual_generation
-            charge_storage!(rng, stors_available, stors_energy,
-                            residual_generation,
-                            view(sys.storages, :, stor_set))
+            residual_generation = charge_storage!(stors_available, stors_energy,
+                                                  residual_generation, stors)
+            sample.regions[1] = RegionResult{L,T,P}(residual_generation, residual_generation, 0.)
 
         else
 
             # Discharge to meet residual_generation shortfall
-            shortfall = discharge_storage!(
-                rng, stors_available, stors_energy,
-                -residual_generation, view(sys.storages, :, stor_set))
+            shortfall = discharge_storage!(stors_available, stors_energy,
+                                           -residual_generation, stors)
+            sample.regions[1] = RegionResult{L,T,P}(shortfall, 0., shortfall)
 
-            # Report remaining shortfall, if any
-            shortfall > 0 && (shortfalls[t] = shortfall)
+        end
 
+        update!(acc, sample, t, i)
+
+    end
+
+
+end
+
+function update_availability!(rng::MersenneTwister, availability::Vector{Bool},
+                              devices::AbstractVector{<:AssetSpec})
+
+    @inbounds for i in 1:length(availability)
+
+        a = assets[i]
+
+        if availability[i] # Unit is online
+            rand(rng) < a.λ && (availability[i] = false) # Unit fails
+        else # Unit is offline
+            rand(rng) < a.μ && (availability[i] = true) # Unit is repaired
         end
 
     end
 
 end
 
-function available_capacity!(rng::MersenneTwister,
-                             gen_availability::Vector{Bool},
-                             generators::AbstractVector{DispatchableGeneratorSpec{T}}
-                             ) where {T <: Real}
+function available_gen_capacity(
+    rng::MersenneTwister,
+    gen_availability::Vector{Bool},
+    generators::AbstractVector{DispatchableGeneratorSpec{T}}
+) where {T <: Real}
 
     capacity = zero(T)
 
     @inbounds for i in 1:length(gen_availability)
-        gen = generators[i]
-        if gen_availability[i]
-            if rand(rng) > gen.λ # Unit doesn't fail, count capacity
-                capacity += gen.capacity
-            else # Unit fails, ignore its capacity
-                gen_availability[i] = false
-            end
-        else
-            if rand(rng) < gen.μ # Unit is repaired, count its capacity
-                gen_availability[i] = true
-                capacity += gen.capacity
-            end
-        end
+        gen_availability[i] && (capacity += generators[i].capacity)
     end
 
     return capacity
 
 end
 
-function charge_storage!(rng::MersenneTwister,
-                         stors_available::Vector{Bool},
+function decay_energy!(stors_energy::Vector{V},
+                       stors::AbstractVector{StorageDeviceSpec{V}}
+) where {V<:Real}
+
+    for (i, stor) in enumerate(stors)
+        stors_energy[i] *= stor.decayrate
+    end
+
+end
+
+function charge_storage!(stors_available::Vector{Bool},
                          stors_energy::Vector{T},
                          surplus::T,
                          stors::AbstractVector{StorageDeviceSpec{T}}
@@ -102,52 +122,28 @@ function charge_storage!(rng::MersenneTwister,
 
     for (i, stor) in enumerate(stors)
 
-        stors_energy[i] *= stor.decayrate
-
         if stors_available[i]
 
-            if rand(rng) > stor.λ # Unit doesn't fail
+            max_charge = stor.energy - stors_energy[i]
 
-                max_charge = stor.energy - stors_energy[i]
+            # TODO: This is wrong, need to consider max charge rate
+            if surplus > max_charge # Fully charge
 
-                if surplus > max_charge # Fully charge
+                stors_energy[i] = stor.energy
+                surplus -= max_charge
 
-                    stors_energy[i] = stor.energy
-                    surplus -= max_charge
+            else # Partially charge
 
-                else # Partially charge
-
-                    stors_energy[i] += surplus
-                    return
-
-                end
-
-            else # Unit fails
-                stors_available[i] = false
-            end
-
-        else
-
-            if rand(rng) < stor.μ # Unit is repaired
-                stors_available[i] = true
-
-                max_charge = stor.energy - stors_energy[i]
-
-                if surplus > max_charge # Fully charge
-
-                    stors_energy[i] = stor.energy
-                    surplus -= max_charge
-
-                else # Partially charge
-
-                    stors_energy[i] += surplus
-                    return
-
-                end
+                stors_energy[i] += surplus
+                return zero(T)
 
             end
+
         end
+
     end
+
+    return surplus
 
 end
 
@@ -156,59 +152,32 @@ function discharge_storage!(rng::MersenneTwister,
                             stors_energy::Vector{T},
                             shortfall::T,
                             stors::AbstractVector{StorageDeviceSpec{T}}
-                            ) where {T <: Real}
+                           ) where {T <: Real}
 
     # TODO: Replace with strategic discharging
     # TODO: Stop assuming hourly periods
 
     for (i, stor) in enumerate(stors)
 
-        stors_energy[i] *= stor.decayrate
-
         if stors_available[i]
 
-            if rand(rng) > stor.λ # Unit doesn't fail
+            # TODO: This is wrong, need to consider max charge rate
+            max_discharge = stors_energy[i]
 
-                max_discharge = stors_energy[i]
+            if shortfall > max_discharge # Fully discharge
 
-                if shortfall > max_discharge # Fully discharge
+                stors_energy[i] = 0
+                shortfall -= max_discharge
 
-                    stors_energy[i] = 0
-                    shortfall -= max_discharge
+            else # Partially discharge
 
-                else # Partially discharge
-
-                    stors_energy[i] -= shortfall
-                    return zero(T)
-
-                end
-
-            else # Unit fails
-                stors_available[i] = false
-            end
-
-        else
-
-            if rand(rng) < stor.μ # Unit is repaired
-
-                stors_available[i] = true
-
-                max_discharge = stors_energy[i]
-
-                if shortfall > max_discharge # Fully discharge
-
-                    stors_energy[i] = 0
-                    shortfall -= max_discharge
-
-                else # Partially discharge
-
-                    stors_energy[i] -= shortfall
-                    return zero(T)
-
-                end
+                stors_energy[i] -= shortfall
+                return zero(T)
 
             end
+
         end
+
     end
 
     return shortfall
