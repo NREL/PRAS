837e641e1a64b74ec290e76383eef875d8bb5346
Gord Stephen <gord.stephen@nrel.gov>
2019-02-25 15:44:01 -0700

    Store sequentialnetworkflow results in SystemOutputStateSample

--- src/simulations/nonsequentialnetworkflow.jl
+++ src/simulations/nonsequentialnetworkflow.jl
@@ -24,8 +24,37 @@ function assess!(acc::ResultAccumulator,
     for i in 1:simulationspec.nsamples
         rand!(acc.rngs[thread], flowproblem, system)
         solveflows!(flowproblem)
-        update!(outputsample, flowproblem)
+        update!(simulationspec, outputsample, flowproblem)
         update!(acc, outputsample, t, i)
+    end
+
+end
+
+function update!(
+    simulationspec::NonSequentialNetworkFlow,
+    sample::SystemOutputStateSample{L,T,P,V},
+    fp::FlowProblem
+) where {L,T<:Period,P<:PowerUnit,V<:Real}
+
+    nregions = length(sample.regions)
+    ninterfaces = length(sample.interfaces)
+
+    # Save gen available, gen dispatched, demand, demand served for each region
+    for i in 1:nregions
+        node = fp.nodes[i]
+        surplus_edge = fp.edges[2*ninterfaces + i]
+        shortfall_edge = fp.edges[2*ninterfaces + nregions + i]
+        sample.regions[i] = RegionResult{L,T,P}(
+            V(node.injection), V(surplus_edge.flow), V(shortfall_edge.flow))
+    end
+
+    # Save flow available, flow for each interface
+    for i in 1:ninterfaces
+        forwardedge = fp.edges[i]
+        forwardflow = forwardedge.flow
+        reverseflow = fp.edges[ninterfaces+i].flow
+        flow = forwardflow > reverseflow ? forwardflow : -reverseflow
+        sample.interfaces[i] = InterfaceResult{L,T,P}(V(forwardedge.limit), V(flow))
     end
 
 end
--- src/simulations/sequentialnetworkflow.jl
+++ src/simulations/sequentialnetworkflow.jl
@@ -78,7 +78,7 @@ function assess!(
             storranges, stors, stors_available,
             flowproblem)
 
-        update!(outputsample, flowproblem)
+        update!(simulationspec, outputsample, flowproblem)
         update!(acc, outputsample, t, i)
 
     end
@@ -193,11 +193,33 @@ function update_energy!(
 
 end
 
+#TODO: This function is very similar to the NonSequentialNetworkFlow
+#      equivalent - refactor to share code?
 function update!(
+    simulationspec::SequentialNetworkFlow,
     outputsample::SystemOutputStateSample{L,T,P,V},
     flowproblem::FlowProblem
 ) where {L,T<:Period,P<:PowerUnit,V<:Real}
 
-    # TODO
+    nregions = length(sample.regions)
+    ninterfaces = length(sample.interfaces)
+
+    # Save gen available, gen dispatched, demand, demand served for each region
+    for i in 1:nregions
+        node = flowproblem.nodes[i]
+        surplus_edge = fp.edges[2*ninterfaces + i]
+        shortfall_edge = fp.edges[2*ninterfaces + 5*nregions + i]
+        sample.regions[i] = RegionResult{L,T,P}(
+            V(node.injection), V(surplus_edge.flow), V(shortfall_edge.flow))
+    end
+
+    # Save flow available, flow for each interface
+    for i in 1:ninterfaces
+        forwardedge = fp.edges[i]
+        forwardflow = forwardedge.flow
+        reverseflow = fp.edges[ninterfaces+i].flow
+        flow = forwardflow > reverseflow ? forwardflow : -reverseflow
+        sample.interfaces[i] = InterfaceResult{L,T,P}(V(forwardedge.limit), V(flow))
+    end
 
 end
