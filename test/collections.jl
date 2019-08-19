d0e163dddd16242fbba7d477bc0fb33fdf614752
Gord Stephen <gord.stephen@nrel.gov>
2019-08-19 15:36:57 -0600

    Bump version to 0.2.0 and add README

---
+++ test/collections.jl
@@ -0,0 +1,37 @@
+@testset "Collections" begin
+
+    @testset "Regions" begin
+
+        Regions{10,MW}(["Region A", "Region B"], rand(1:10, 2, 10))
+
+        @test_throws AssertionError Regions{10,MW}(
+            ["Region A", "Region B"], rand(1:10, 2, 5))
+
+        @test_throws AssertionError Regions{10,MW}(
+            ["Region A", "Region B"], rand(1:10, 3, 10))
+
+        @test_throws AssertionError Regions{10,MW}(
+            ["Region A", "Region B"], -rand(1:10, 2, 10))
+
+    end
+
+    @testset "Interfaces" begin
+
+        Interfaces{10,MW}(
+            [1,1,2], [2,3,3], rand(1:15, 3, 10), rand(1:15, 3, 10))
+
+        @test_throws AssertionError Interfaces{10,MW}(
+            [1,1,2], [2,3], rand(1:15, 3, 10), rand(1:15, 3, 10))
+
+        @test_throws AssertionError Interfaces{10,MW}(
+            [1,1,2], [2,3,3], rand(1:15, 2, 10), rand(1:15, 2, 10))
+
+        @test_throws AssertionError Interfaces{10,MW}(
+            [1,1,2], [2,3,3], rand(1:15, 3, 11), rand(1:15, 3, 11))
+
+        @test_throws AssertionError Interfaces{10,MW}(
+            [1,1,2], [2,3,3], rand(1:15, 3, 10), -rand(1:15, 3, 10))
+
+    end
+
+end
