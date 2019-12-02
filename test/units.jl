80ad2e1ca6435567924742d54a0d3a7a0fb0b16e
Gord Stephen <gord.stephen@nrel.gov>
2019-12-02 10:53:23 -0800

    Add extra tests

--- test/units.jl
+++ test/units.jl
@@ -6,4 +6,16 @@
     @test energytopower(100, MWh, 10, Hour, MW) == 10
     @test energytopower(100, MWh, 30, Minute, MW) == 200
 
+    @test unitsymbol(MW) == "MW"
+    @test unitsymbol(GW) == "GW"
+
+    @test unitsymbol(MWh) == "MWh"
+    @test unitsymbol(GWh) == "GWh"
+    @test unitsymbol(TWh) == "TWh"
+
+    @test unitsymbol(Minute) == "min"
+    @test unitsymbol(Hour) == "h"
+    @test unitsymbol(Day) == "d"
+    @test unitsymbol(Year) == "y"
+
 end
