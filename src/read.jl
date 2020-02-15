7c4e30c98aa6a12090724b52b70bff5aed97a975
Gord Stephen <gord.stephen@nrel.gov>
2020-02-14 19:51:00 -0800

    Start reading in from HDF5

--- src/read.jl
+++ src/read.jl
@@ -6,13 +6,14 @@ Load a `SystemModel` from an appropriately-formatted HDF5 file on disk.
 """
 function SystemModel(inputfile::String)
 
-    system = read(inputfile) do f::HDF5File
+    system = h5open(inputfile, "r") do f::HDF5File
 
         version, versionstring = getversion(f)
+        println("$versionstring = $version")
 
         # Determine the appropriate version of the constructor to use 
-        return if (0,2,2) <= version < (0,3,0)
-            SystemModel(f, Val((0,2,2)))
+        return if (0,2,0) <= version < (0,3,0)
+            systemmodel_0_2_0(f)
         else
             @error("File format $versionstring not supported by this version of PRASBase.")
         end
@@ -24,9 +25,37 @@ function SystemModel(inputfile::String)
 end
 
 
-function SystemModel(f::HDF5File, ::Val{(0,2,2)})
-    # create SystemModel
-    # return SystemModel
+function systemmodel_0_2_0(f::HDF5File)
+
+    metadata = attrs(f)
+
+     start_timestamp = ZonedDateTime(read(metadata["start_timestamp"]),
+                                     dateformat"yyyy-mm-ddTHH:MM:SSZ")
+
+     N = read(metadata["timestep_count"])
+     L = read(metadata["timestep_length"])
+     T = timeunits[read(metadata["timestep_unit"])]
+     P = powerunits[read(metadata["power_unit"])]
+     E = energyunits[read(metadata["energy_unit"])]
+
+    # Load all data
+
+    regions = Regions{N,P}()
+    generators = Generators{N,L,T,P}()
+    storages = Storages{N,L,T,P,E}()
+    generatorstorages = GeneratorStorages{N,L,T,P,E}()
+
+    interfaces = Interfaces{N,P}()
+    lines = Lines{N,L,T,P}()
+
+    return SystemModel{N,L,T,P,E}(
+        regions, interfaces,
+        generators, region_gen_idxs,
+        storages, region_stor_idxs,
+        generatorstorages, region_genstor_idxs,
+        lines, interface_line_idxs,
+        timestamps)
+
 end
 
 """
@@ -35,13 +64,17 @@ Errors if the label cannot be found or parsed as expected.
 """
 function getversion(f::HDF5File)
 
-    # TODO
-
-    error("File format version indicator could not be found - the file may " *
+    exists(attrs(f), "pras_dataversion") || error(
+          "File format version indicator could not be found - the file may " *
           "not be a PRAS SystemModel representation.")
 
-    error("File format version $versionstring not recognized")
+    versionstring = read(attrs(f)["pras_dataversion"])
 
-    return versionstring, (major, minor, patch)
+    version = match(r"^v(\d+)\.(\d+)\.(\d+)$", versionstring)
+    isnothing(version) && error("File format version $versionstring not recognized")
+
+    major, minor, patch = parse.(Int, version.captures)
+
+    return (major, minor, patch), versionstring
 
 end
