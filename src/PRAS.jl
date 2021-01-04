5aee1cb8ba2807fec4ccec84c0f04bdcad3c9052
scdhulipala <sdhulipa@nrel.gov>
2021-01-04 12:08:47 -0700

    Add savemodel for exporting .pras files

--- src/PRAS.jl
+++ src/PRAS.jl
@@ -2,6 +2,8 @@ module PRAS
 
 using Reexport
 
+const PRAS_VERSION = "v0.5.4"
+
 include("PRASBase/PRASBase.jl")
 include("ResourceAdequacy/ResourceAdequacy.jl")
 include("CapacityCredit/CapacityCredit.jl")
