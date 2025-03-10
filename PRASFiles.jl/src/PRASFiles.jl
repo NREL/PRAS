dea39b30e39ca47f98936886d81371c379e000eb
Surya Chandan Dhulipala <suryachandan.dhulipala@nrel.gov>
2025-03-10 13:37:15 -0600

    Support exporting aggregated shortfall results to JSON (#67)

--- PRASFiles.jl/src/PRASFiles.jl
+++ PRASFiles.jl/src/PRASFiles.jl
@@ -6,27 +6,35 @@ import PRASCore.Systems: SystemModel, Regions, Interfaces,
                          Generators, Storages, GeneratorStorages, Lines,
                          timeunits, powerunits, energyunits, unitsymbol
 
-import Dates: @dateformat_str
+import PRASCore.Results: EUE, LOLE, NEUE, ShortfallResult, ShortfallSamplesResult, AbstractShortfallResult, Result
+import StatsBase: mean
+import Dates: @dateformat_str, format, now
 import TimeZones: ZonedDateTime
 import HDF5: HDF5, attributes, File, Group, Dataset, Datatype, dataspace,
              h5open, create_group, create_dataset, hdf5_type_id
 import HDF5.API: h5t_create, h5t_copy, h5t_insert, h5t_set_size,
                  H5T_COMPOUND, h5d_write, H5S_ALL, H5P_DEFAULT
 
-export savemodel
+import StructTypes: StructType, Struct, OrderedStruct
+import JSON3: pretty
 
-include("read.jl")
-include("write.jl")
-include("utils.jl")
+export savemodel
+export saveshortfall
+
+include("Systems/read.jl")
+include("Systems/write.jl")
+include("Systems/utils.jl")
+include("Results/utils.jl")
+include("Results/write.jl")
 
 function toymodel()
     path = dirname(@__FILE__)
-    return SystemModel(path * "/toymodel.pras")
+    return SystemModel(joinpath(path, "Systems","toymodel.pras")) 
 end
 
 function rts_gmlc()
     path = dirname(@__FILE__)
-    return SystemModel(path * "/rts.pras")
+    return SystemModel(joinpath(path, "Systems","rts.pras")) 
 end
 
 end
--- PRASFiles.jl/test/runtests.jl
+++ PRASFiles.jl/test/runtests.jl
@@ -1,6 +1,7 @@
 using PRASCore
 using PRASFiles
 using Test
+using JSON3
 
 @testset "PRASFiles" begin
 
@@ -25,6 +26,47 @@ using Test
 
         assess(PRASFiles.rts_gmlc(), SequentialMonteCarlo(samples=100), Shortfall())
 
+    end
+
+    @testset "Save Aggregate Results" begin
+        rts_sys = PRASFiles.rts_gmlc()
+        # Make load in all regions in rts_sys 10 times the original load for meaningful results
+        for i in 1:length(rts_sys.regions.names)
+            rts_sys.regions.load[i, :] = 10 * rts_sys.regions.load[i, :]
+        end
+        results = assess(rts_sys, SequentialMonteCarlo(samples=10, threaded = false, seed = 1), Shortfall(), ShortfallSamples(), Surplus());
+        shortfall = results[1];
+        path = joinpath(dirname(@__FILE__),"PRAS_Results_Export");
+        exp_location_1 = PRASFiles.saveshortfall(shortfall, rts_sys, path);
+        @test isfile(joinpath(exp_location_1, "pras_results.json"))
+        exp_results_1 = JSON3.read(joinpath(exp_location_1, "pras_results.json"), PRASFiles.SystemResult)
+        @test exp_results_1.lole.mean == PRASCore.LOLE(shortfall).lole.estimate
+        @test exp_results_1.eue.mean == PRASCore.EUE(shortfall).eue.estimate
+        @test exp_results_1.neue.mean == PRASCore.NEUE(shortfall).neue.estimate
+        @test exp_results_1.region_results[1].lole.mean == PRASCore.LOLE(shortfall, exp_results_1.region_results[1].name).lole.estimate
+        @test exp_results_1.region_results[1].eue.mean == PRASCore.EUE(shortfall, exp_results_1.region_results[1].name).eue.estimate
+        @test exp_results_1.region_results[1].neue.mean == PRASCore.NEUE(shortfall, exp_results_1.region_results[1].name).neue.estimate
+
+        shortfall_samples = results[2];
+        exp_location_2 = PRASFiles.saveshortfall(shortfall_samples, rts_sys, path);
+        @test isfile(joinpath(exp_location_2, "pras_results.json"))
+        exp_results_2 = JSON3.read(joinpath(exp_location_2, "pras_results.json"), PRASFiles.SystemResult)
+        @test exp_results_2.lole.mean == PRASCore.LOLE(shortfall_samples).lole.estimate
+        @test exp_results_2.eue.mean == PRASCore.EUE(shortfall_samples).eue.estimate
+        @test exp_results_2.neue.mean == PRASCore.NEUE(shortfall_samples).neue.estimate
+        @test exp_results_2.region_results[1].lole.mean == PRASCore.LOLE(shortfall_samples, exp_results_2.region_results[1].name).lole.estimate
+        @test exp_results_2.region_results[1].eue.mean == PRASCore.EUE(shortfall_samples, exp_results_2.region_results[1].name).eue.estimate
+        @test exp_results_2.region_results[1].neue.mean == PRASCore.NEUE(shortfall_samples, exp_results_2.region_results[1].name).neue.estimate
+
+        @test exp_results_1.lole.mean ≈ exp_results_2.lole.mean
+        @test exp_results_1.eue.mean ≈ exp_results_2.eue.mean
+        @test exp_results_1.neue.mean ≈ exp_results_2.neue.mean
+        @test exp_results_1.region_results[1].lole.mean ≈ exp_results_2.region_results[1].lole.mean
+        @test exp_results_1.region_results[1].eue.mean ≈ exp_results_2.region_results[1].eue.mean
+        @test exp_results_1.region_results[1].neue.mean ≈ exp_results_2.region_results[1].neue.mean
+
+        surplus = results[3]
+        @test_throws "saveshortfall is not implemented for" PRASFiles.saveshortfall(surplus, rts_sys, path)
     end
 
 end
