85bd0ef2d53a048749d3b8b7b972158bf62d2634
juflorez <Julian.Florez@nrel.gov>
2025-07-01 14:38:18 -0600

    fix test DR inputs

--- PRASCore.jl/test/Systems/assets.jl
+++ PRASCore.jl/test/Systems/assets.jl
@@ -135,23 +135,23 @@
 
         DemandResponses{10,1,Hour,MW,MWh}(
             names, categories, vals_int, vals_int, vals_int,
-            vals_float, vals_float, vals_float, vals_float, vals_float)
+            vals_float, vals_float, vals_float, vals_int, vals_float, vals_float)
 
         @test_throws AssertionError DemandResponses{5,1,Hour,MW,MWh}(
             names, categories, vals_int, vals_int, vals_int,
-            vals_float, vals_float, vals_float, vals_float, vals_float)
+            vals_float, vals_float, vals_float, vals_int, vals_float, vals_float)
 
         @test_throws AssertionError DemandResponses{10,1,Hour,MW,MWh}(
             names, categories[1:2], vals_int, vals_int, vals_int,
-            vals_float, vals_float, vals_float, vals_float, vals_float)
+            vals_float, vals_float, vals_float, vals_int, vals_float, vals_float)
 
         @test_throws AssertionError DemandResponses{10,1,Hour,MW,MWh}(
             names[1:2], categories[1:2], vals_int, vals_int, vals_int,
-            vals_float, vals_float, vals_float, vals_float, vals_float)
+            vals_float, vals_float, vals_float, vals_int, vals_float, vals_float)
 
         @test_throws AssertionError DemandResponses{10,1,Hour,MW,MWh}(
             names, categories, vals_int, vals_int, vals_int,
-            vals_float, vals_float, -vals_float, vals_float, vals_float)
+            vals_float, vals_float, -vals_float, vals_int, vals_float, vals_float)
 
     end
 
