5cbba37d286376fe35755902d1e23751d68d4ec7
Dhulipala <sdhulipa@nrel.gov>
2025-08-07 13:19:12 -0600

    Add SystemModel tests

--- PRASCore.jl/test/Systems/SystemModel.jl
+++ PRASCore.jl/test/Systems/SystemModel.jl
@@ -19,11 +19,22 @@
 
     tz = tz"UTC"
     timestamps = ZonedDateTime(2020, 1, 1, 0, tz):Hour(1):ZonedDateTime(2020,1,1,9, tz)
+    attrs = Dict("type" => "Single-Region System")
 
     # Single-region constructor
-    SystemModel(
-        generators, storages, generatorstorages, timestamps, rand(1:20, 10))
+    single_reg_sys = SystemModel(
+        generators, storages, generatorstorages, timestamps, rand(1:20, 10), attrs)
+    @test single_reg_sys isa SystemModel
 
+    io = IOBuffer()
+    show(io, "text/plain", single_reg_sys)
+    text = String(take!(io))
+    @test occursin("PRAS system with 1 regions,", text)
+    @test occursin("Region names:", text)
+    @test occursin("Assets:", text)
+    @test occursin("Time series:", text)
+    @test occursin("Attributes:", text)
+    
     regions = Regions{10,MW}(
         ["Region A", "Region B"], rand(1:20, 2, 10))
 
@@ -40,11 +51,44 @@
     line_interfaces = [1:2]
 
     # Multi-region constructor
-    SystemModel(
+    multi_region_sys_wo_attrs = SystemModel(
         regions, interfaces,
         generators, gen_regions, storages, stor_regions,
         generatorstorages, genstor_regions,
         lines, line_interfaces,
         timestamps)
+    @test multi_region_sys_wo_attrs isa SystemModel
 
+    attrs = Dict("type" => "Multi-Region System")
+    multi_region_sys_with_attrs = SystemModel(
+        regions, interfaces,
+        generators, gen_regions, storages, stor_regions,
+        generatorstorages, genstor_regions,
+        lines, line_interfaces,
+        timestamps, attrs)
+    @test multi_region_sys_with_attrs isa SystemModel
+    
+    io = IOBuffer()
+    show(io, "text/plain", multi_region_sys_with_attrs)
+    text = String(take!(io))
+    @test occursin("PRAS system with 2 regions, and 1 interfaces between these regions.", text)
+    @test occursin("Region names:", text)
+    @test occursin("Generators: 2", text)
+    @test occursin("Storages: 2", text)
+    @test occursin("GeneratorStorages: 1", text)
+    @test occursin("Lines: 2", text)
+    @test occursin("Resolution: 1", text)
+    @test occursin("Resolution: 1", text)
+    @test occursin("Time zone: UTC", text)
+    @test occursin("type: Multi-Region System", text)
+
+    attrs = Dict("type" => "Multi-Region System", "year" => 2025)
+    @test_throws MethodError multi_region_sys_with_attrs = SystemModel(
+        regions, interfaces,
+        generators, gen_regions, storages, stor_regions,
+        generatorstorages, genstor_regions,
+        lines, line_interfaces,
+        timestamps, attrs)
+    
 end
+
