3b5ba7f35df1fcab705770fd32ef5fd5e72eee60
Srihari Sundar <sriharisundar95@gmail.com>
2025-09-30 11:35:01 -0600

    Add charts, option to export the underlying DB, report title

--- PRASReport.jl/src/report.jl
+++ PRASReport.jl/src/report.jl
@@ -3,14 +3,25 @@
 # end
 
 """
-    create_html_report(conn::DuckDB.DuckDBConnection; 
-                          output_path::String="report.html", 
-                          title::String="PRAS Report")
+    create_html_report(sf::ShortfallResult, flow::FlowResult;
+                          report_name::String="report",
+                          threshold::Int=0,
+                          title::String="Resource Adequacy Report")
+
+Create an HTML report from PRAS simulation results.
+
+# Fields
+- `sf::ShortfallResult`: Shortfall simulation results
+- `flow::FlowResult`: Flow simulation results
+- `report_name::String`: Base name for the generated HTML file (default: "report")
+- `threshold::Int`: Event threshold for filtering events (default: 0)
+- `title::String`: Title to display in the report header (default: "Resource Adequacy Report")
 """
 function create_html_report(sf::ShortfallResult,
                             flow::FlowResult;
                             report_name::String="report",
-                            threshold::Int=0)
+                            threshold::Int=0,
+                            title::String="Resource Adequacy Report")
     
     tempdb_path = tempname() * ".db"
     dbfile = DuckDB.open(tempdb_path)
@@ -29,6 +40,8 @@ function create_html_report(sf::ShortfallResult,
                             "        // Placeholder for base64 database - this will be replaced by Julia" => "")
     report_html = replace(report_html, 
                             "const BASE64_DB = \"{{BASE64_DB_PLACEHOLDER}}\"" => "const BASE64_DB = \"$(base64_db)\"")
+    report_html = replace(report_html, 
+                            "{{REPORT_TITLE_PLACEHOLDER}}" => title)
     
     write(report_name * ".html", report_html)
 
