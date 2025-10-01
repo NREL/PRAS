4d7b6e5fda1eec1b1a581441d25c02a05cd2f6f8
Srihari Sundar <sriharisundar95@gmail.com>
2025-10-01 07:37:35 -0600

    Add tests!

---
+++ PRASReport.jl/test/report.jl
@@ -0,0 +1,42 @@
+@testset "String input to create_html_report" begin
+    system_path = joinpath(@__DIR__, "../../PRASFiles.jl/src/Systems/rts.pras")
+
+    string_test = @capture_out begin
+        create_html_report(system_path; samples=1000, seed=1, 
+                            report_name="string_test")
+    end
+    string_test = strip(string_test)
+    @test startswith(string_test,"Writing report to:")
+    @test contains(string_test, "string_test.html")
+    string_test = replace(string_test, r"Writing report to: " => "")
+    @test isfile(string_test)
+    rm(string_test; force=true)
+end
+
+@testset "SystemModel input to create_html_report" begin
+    string_test = @capture_out begin
+        create_html_report(system; samples=1000, seed=1, 
+                            report_name="sysmodel_test")
+    end
+    string_test = strip(string_test)
+    @test startswith(string_test,"Writing report to:")
+    @test contains(string_test, "sysmodel_test.html")
+    string_test = replace(string_test, r"Writing report to: " => "")
+    @test isfile(string_test)
+    rm(string_test; force=true)
+end
+
+@testset "ShortfallResult, FlowResult input to create_html_report" begin
+    sf,flow = assess(system,SequentialMonteCarlo(samples=1000,seed=1),Shortfall(),Flow());
+    
+    string_test = @capture_out begin
+        create_html_report(sf,flow;  
+                            report_name="sfflow_test")
+    end
+    string_test = strip(string_test)
+    @test startswith(string_test,"Writing report to:")
+    @test contains(string_test, "sfflow_test.html")
+    string_test = replace(string_test, r"Writing report to: " => "")
+    @test isfile(string_test)
+    rm(string_test; force=true)
+end
--- PRASReport.jl/test/runtests.jl
+++ PRASReport.jl/test/runtests.jl
@@ -1,44 +1,23 @@
 using Test
-using PRASReport
-using TimeZones
 using Dates
+using PRASReport
+using PRASCore
+using PRASFiles
+using TimeZones
+using Suppressor
 
 # Test copperplate
 # Test get functions from database for wrong names
+system = rts_gmlc()
 
 @testset "PRASReport.jl Tests" begin
     @testset "Test events" begin
-        
-        eue = [EUE{2,1,Hour,MWh}(MeanEstimate(1.2)), 
-               EUE{2,1,Hour,MWh}(MeanEstimate(0.6)),
-               EUE{2,1,Hour,MWh}(MeanEstimate(0.5)),
-               EUE{2,1,Hour,MWh}(MeanEstimate(0.1)),
-        ]
-
-        lole = [LOLE{4380,2,Hour}(MeanEstimate(0.13)),
-                LOLE{4380,2,Hour}(MeanEstimate(0.08)),
-                LOLE{4380,2,Hour}(MeanEstimate(0.06)),
-                LOLE{4380,2,Hour}(MeanEstimate(0.04))
-
-        ]
-
-        @test event_length(event) == Hour(5)
+        include("events.jl")
     end
 
-    @testset "Test get_events" begin
-        
-        #TODO: Test for empty sf object
+    @testset "Test html report generation" begin
 
-    end
-    @testset "Test get_stepranges" begin
+        include("report.jl")
 
-        timestamps = ZonedDateTime.(DateTime(2023,1,1):Hour(1):DateTime(2023,1,10), tz"UTC")
-        selected_times = [timestamps[2], timestamps[3], timestamps[5], timestamps[6], timestamps[7], timestamps[9]]
-        step_ranges = get_stepranges(selected_times, Hour(1), Hour(1))
-
-        @test length(step_ranges) == 3 "Incorrect number of step ranges"
-        @test step_ranges[1] == (timestamps[2]:Hour(1):timestamps[3]) "First step range incorrect"
-        @test step_ranges[2] == (timestamps[5]:Hour(1):timestamps[7]) "Second step range incorrect"
-        @test step_ranges[3] == (timestamps[9]:Hour(1):timestamps[9]) "Third step range incorrect"
     end
 end
